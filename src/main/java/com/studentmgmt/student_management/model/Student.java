efc60a35537b88dbd0949ef183e17c273d947752
Gunaseelanselvam <gunaseelanvec@gmail.com>
2026-06-26 16:13:34 +0530

    student class created

---
+++ src/main/java/com/studentmgmt/student_management/model/Student.java
@@ -0,0 +1,32 @@
+package com.studentmgmt.student_management.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String department;
+    private Double cgpa;
+    
+}
