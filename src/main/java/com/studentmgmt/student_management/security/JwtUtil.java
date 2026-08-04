642ee661944a94a21515e8117b1b1d0e5ce5810f
Gunaseelanselvam <gunaseelanvec@gmail.com>
2026-08-04 12:58:15 +0530

    jwutil

---
+++ src/main/java/com/studentmgmt/student_management/security/JwtUtil.java
@@ -0,0 +1,41 @@
+package com.studentmgmt.student_management.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.stereotype.Component;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+@Component
+public class JwtUtil{
+
+    private final String SECRET_KEY = "myStudentApp2026SuperSecretJwtSigningKeyDoNotShareThisAnywhere";
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 *60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+
+    }
+
+    public String extractUsername(String token){
+
+        return Jwts.parser()
+                .setSigingKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        String username = extractUsername(token);
+        return username.equals(userDetails.getUsername());
+    }
+
+}
