5fce116af03d7f701b05ca7b4a45dceb83b9b521
Gunaseelanselvam <gunaseelanvec@gmail.com>
2026-08-04 18:25:11 +0530

    update security

--- src/main/java/com/studentmgmt/student_management/security/SecurityConfig.java
+++ src/main/java/com/studentmgmt/student_management/security/SecurityConfig.java
@@ -3,17 +3,26 @@ package com.studentmgmt.student_management.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
 
 
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfig{
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter){
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
     
     @Bean 
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -27,6 +36,7 @@ public class SecurityConfig{
                 .sessionManagement(session -> session
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     );
+         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
          return http.build();
     }
 
@@ -35,5 +45,9 @@ public class SecurityConfig{
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
 
 }
